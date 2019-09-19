5db1063f3183687481f5ae59a58a5c1fe1065396
Ta Tuan <tatuanmac@gmail.com>
2019-09-19 13:06:03 +0700

    base project

---
+++ ICLEdu/ICLEdu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ICLEdu
+//
+//  Created by Duc Nguyen on 2019/9/19.
+//  Copyright © 2019 Ta Tuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
