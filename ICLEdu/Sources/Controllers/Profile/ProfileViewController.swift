c61d0c2f9e4ef2bb6a7b742deda5b8427845346b
Tuấn Tạ <tatuanmac@gmail.com>
2019-10-28 19:37:39 +0700

    tắt lựa chọn màn hình profile

--- ICLEdu/Sources/Controllers/Profile/ProfileViewController.swift
+++ ICLEdu/Sources/Controllers/Profile/ProfileViewController.swift
@@ -15,7 +15,6 @@ class ProfileViewController: UITableViewController {
     let noteCell = "NoteCell"
     
     let presenterProfile = PresenterProfile()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,8 +22,8 @@ class ProfileViewController: UITableViewController {
         
         
         tableView.register(UINib(nibName: profileCell, bundle: nil), forCellReuseIdentifier: profileCell)
-//        tableView.register(UINib(nibName: processCell, bundle: nil), forCellReuseIdentifier: processCell)
-//        tableView.register(UINib(nibName: noteCell, bundle: nil), forCellReuseIdentifier: noteCell)
+        //        tableView.register(UINib(nibName: processCell, bundle: nil), forCellReuseIdentifier: processCell)
+        //        tableView.register(UINib(nibName: noteCell, bundle: nil), forCellReuseIdentifier: noteCell)
     }
     
     func getDataProfile(profileData: ProfileModel?) {
@@ -36,34 +35,34 @@ class ProfileViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if indexPath.row == 0{
-            let cell = tableView.dequeueReusableCell(withIdentifier: profileCell) as! ProfileCell
-            cell.setDataProfile(name: presenterProfile.profileModel?.name,
-                                address: presenterProfile.profileModel?.address,
-                                gender: presenterProfile.profileModel?.gender,
-                                dateOfBirth: presenterProfile.profileModel?.birthday,
-                                phone: presenterProfile.profileModel?.phone)
-            return cell
-//        }
+        //        if indexPath.row == 0{
+        let cell = tableView.dequeueReusableCell(withIdentifier: profileCell) as! ProfileCell
+        cell.setDataProfile(name: presenterProfile.profileModel?.name,
+                            address: presenterProfile.profileModel?.address,
+                            gender: presenterProfile.profileModel?.gender,
+                            dateOfBirth: presenterProfile.profileModel?.birthday,
+                            phone: presenterProfile.profileModel?.phone)
+        cell.isUserInteractionEnabled = false
+        return cell
+        //        }
         
-//        if indexPath.row == 1{
-//            let cell = tableView.dequeueReusableCell(withIdentifier: processCell) as! ProcessCell
-//            return cell
-//        }
-//
-//        let cell = tableView.dequeueReusableCell(withIdentifier: noteCell) as! NoteCell
-//        return cell
+        //        if indexPath.row == 1{
+        //            let cell = tableView.dequeueReusableCell(withIdentifier: processCell) as! ProcessCell
+        //            return cell
+        //        }
+        //
+        //        let cell = tableView.dequeueReusableCell(withIdentifier: noteCell) as! NoteCell
+        //        return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        if indexPath.row == 0{
-            return tableView.frame.width
-//        }
-//        if indexPath.row == 1{
-//            return tableView.frame.width
-//        }
-//        return 85
+        //        if indexPath.row == 0{
+        return tableView.frame.width + 15
+        //        }
+        //        if indexPath.row == 1{
+        //            return tableView.frame.width
+        //        }
+        //        return 85
     }
-    
 }
 
