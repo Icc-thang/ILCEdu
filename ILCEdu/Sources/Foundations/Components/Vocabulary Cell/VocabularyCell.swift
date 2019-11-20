d515038dcce1e397847185b3f9fa4f58fe510aea
Tuấn Tạ <tatuanmac@gmail.com>
2019-11-20 10:18:47 +0700

    fix constraint vocabulary cell & card cell

--- ILCEdu/Sources/Foundations/Components/Vocabulary Cell/VocabularyCell.swift
+++ ILCEdu/Sources/Foundations/Components/Vocabulary Cell/VocabularyCell.swift
@@ -109,7 +109,7 @@ extension VocabularyCell : UICollectionViewDelegate, UICollectionViewDataSource,
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.width - 16 
+        let width = collectionView.frame.width - 16
         return CGSize(width: width, height: width)
     }
     
